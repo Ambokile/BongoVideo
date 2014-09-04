8eef730e08832e3ed61f8a36166305f1f2804da1
Ambokile <ambokilekifukwe@gmail.com>
2014-09-04 01:36:27 -0700

    test

--- js/processor.js
+++ js/processor.js
@@ -1,4 +1,33 @@
 // JavaScript Document
+ document.addEventListener("deviceready",onDeviceReady,false);
+	function grap_info(){
+			var device_id = device.uuid;
+	 var xmlhttp;
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();}
+	else{
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");}
+ xmlhttp.onreadystatechange = function(){
+	 if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+		document.querySelector("#info").innerHTML = xmlhttp.responseText;
+	 } 
+			 
+			                       };
+xmlhttp.open("GET","http://fishpond.site88.net/get_video.php?id="+device_id,true);
+xmlhttp.send();
+   }
+    
+
+function onDeviceReady(){
+     pictureSource=navigator.camera.PictureSourceType;
+     destinationType=navigator.camera.DestinationType;
+         
+             setTimeout(grap_info,10);
+			 
+          }
+       
+	
+   
 	
 
       
