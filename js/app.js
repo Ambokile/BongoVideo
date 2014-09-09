7bb29d0302d06aecf5896c10977e91258211eb39
Ambokile <ambokilekifukwe@gmail.com>
2014-09-09 07:21:31 -0700

    test

--- js/app.js
+++ js/app.js
@@ -28,10 +28,22 @@ application.controller('InfoCtrl', function($scope) {
   };
 }
  
-  function MyCtrl($scope, $ionicSlideBoxDelegate) {
-  $scope.nextSlide = function() {
-    $ionicSlideBoxDelegate.next();
+application.directive('fadeBar', function($timeout) {
+  return {
+    restrict: 'E',
+    template: '<div class="fade-bar"></div>',
+    replace: true,
+    link: function($scope, $element, $attr) {
+      // Run in the next scope digest
+      $timeout(function() {
+        // Watch for changes to the openRatio which is a value between 0 and 1 that says how "open" the side menu is
+        $scope.$watch('sideMenuController.getOpenRatio()', function(ratio) {
+          // Set the transparency of the fade bar
+          $element[0].style.opacity = Math.abs(ratio);
+        });
+      });
+    }
   }
-}
+});
  
  
