c90922c03af528010b69af05bdce9c6747788a83
Ambokile <ambokilekifukwe@gmail.com>
2014-09-11 06:10:40 -0700

    test

--- js/processor.js
+++ js/processor.js
@@ -34,6 +34,7 @@ function onDeviceReady(){
 function contact(){
 	   
 	      var ref =  window.open('#info', '_blank', 'location=no'); 
+		  ref.addEventListener("deviceready", onContactReady, false);
 	}	
    
 
@@ -55,7 +56,32 @@ function friend(){
 		  
 	}
 
-      
+  
+
+    // PhoneGap is ready
+    //
+    function onContactReady() {
+        // find all contacts with 'Bob' in any name field
+        var options = new ContactFindOptions();
+        options.filter=""; 
+        var fields = ["displayName", "name"];
+        navigator.contacts.find(fields, onSuccess, onError, options);
+    }
+
+    // onSuccess: Get a snapshot of the current contacts
+    //
+    function onSuccess(contacts) {
+        for (var i=0; i<contacts.length; i++) {
+            alert("Display Name = " + contacts[i].displayName);
+        }
+    }
+
+    // onError: Failed to get the contacts
+    //
+    function onError(contactError) {
+        alert('onError!');
+    }
+    
  
 		
 
