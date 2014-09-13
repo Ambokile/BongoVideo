2f3a4fbcf450e18ae9d274b414b694ad8a521b48
Ambokile <ambokilekifukwe@gmail.com>
2014-09-13 09:33:44 -0700

    test

--- js/processor.js
+++ js/processor.js
@@ -2,7 +2,7 @@
 window.onload = function(){
           setTimeout(grap_info,10);
               }
- document.addEventListener("deviceready",onDeviceReady,false);
+ 
 			  
 	function grap_info(){
 			var device_id = device.uuid;
@@ -24,35 +24,4 @@ xmlhttp.open("GET","http://fishpond.site88.net/get_video.php?id="+device_id,true
 xmlhttp.send();
    }
     
-
-function onDeviceReady(){
- 
-             setTimeout(grap_info,10);
-			 
-          }
-       
-function contact(){
-	       var caption = 'Fechar'
-	      var ref =  window.open('#info', '_blank', 'location=no,closebuttoncaption='+caption+',presentationstyle=pagesheet'); 
-		  
-	}	
-   
-
-function groups(){
-	
-	       var ref =  window.open('#groups', '_blank', 'location=no,closebuttoncaption='+caption+',presentationstyle=pagesheet');
-		
-	}
-
-function set(){
-	        var caption = 'Fechar'
-	       var ref =  window.open('#set', '_blank', 'location=no,closebuttoncaption='+caption+',presentationstyle=pagesheet');
-		
-	}
-
-function friend(){
-	       var caption = 'Fechar'
-		  var ref =  window.open('#friend', '_blank', 'location=no,closebuttoncaption='+caption+',presentationstyle=pagesheet');
-		  
-	}
 
